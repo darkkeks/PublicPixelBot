52b400d4c8f94e18c7570f0a10e5b81782623fd1
DarkKeks <darkkeks@rambler.ru>
2017-11-10 01:42:58 +0300

    New template

--- PixelBot.user.js
+++ PixelBot.user.js
@@ -15,7 +15,7 @@ function CoiPixelBot() {
 
     CoiPixelBot.url = {
         script: window.botUrl,
-        image:  'https://i.imgur.com/f3MZnz3.png'
+        image:  'https://i.imgur.com/a96J316.png'
     };
 
     CoiPixelBot.refreshTime = 300;
